0d3217a302e615d50a313403c46e22f643659dda
Selu Reguera <reguera.alvarez@gmail.com>
2023-05-02 07:37:32 +0200

    actualización

--- NOTEBOOK/Querys Videoclub.ps1
+++ NOTEBOOK/Querys Videoclub.ps1
@@ -1,3 +1,69 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema archivos_videoclub3
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS archivos_videoclub3 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE archivos_videoclub3 ;
+
+-- Table archivos_videoclub3.INVENTORY
+
+CREATE TABLE IF NOT EXISTS INVENTORY (
+inventory_id INT NOT NULL,
+FILMS_title_id INT NULL DEFAULT NULL,
+PRIMARY KEY (inventory_id),
+FOREIGN KEY (FILMS_title_id)
+REFERENCES FILMS (title_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+-- Table archivos_videoclub3.FILMS
+
+CREATE TABLE IF NOT EXISTS FILMS (
+title_id INT NOT NULL,
+title VARCHAR(45) NULL DEFAULT NULL,
+description_ VARCHAR(5000) NULL DEFAULT NULL,
+length_ INT NULL DEFAULT NULL,
+replacement_cost FLOAT NULL DEFAULT NULL,
+actors VARCHAR(5000) NULL DEFAULT NULL,
+PRIMARY KEY (title_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+-- Table archivos_videoclub3.RENTAL
+
+CREATE TABLE IF NOT EXISTS RENTAL (
+rental_id INT NOT NULL,
+rental_date DATETIME NULL DEFAULT NULL,
+inventory_id INT NULL DEFAULT NULL,
+customer_id INT NULL DEFAULT NULL,
+return_date DATETIME NULL DEFAULT NULL,
+PRIMARY KEY (rental_id),
+FOREIGN KEY (INVENTORY_inventory_id)
+REFERENCES INVENTORY (inventory_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+FOREIGN KEY (customer_id)
+REFERENCES CLIENTS (customer_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+-- Table archivos_videoclub3.CLIENTS
+
+CREATE TABLE IF NOT EXISTS CLIENTS (
+customer_id INT NOT NULL,
+name_ VARCHAR(45) NULL DEFAULT NULL,
+address VARCHAR(500) NULL DEFAULT NULL,
+phone INT NULL,
+PRIMARY KEY (customer_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+
 QUERY 1
 
 Tres películas en las que aparezca la actriz Penélope Guiness:
